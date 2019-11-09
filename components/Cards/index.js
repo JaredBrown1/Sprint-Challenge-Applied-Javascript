8e2d26109a30f8a972fdcbb6e7ba4a469a6619d2
Jared Brown <jared@Jareds-MBP.lan1>
2019-11-09 11:19:52 -0800

    finished cards part

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,59 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// const axiosPromises = axios.get('https://lambda-times-backend.herokuapp.com/articles');
+// console.log(axiosPromises);
+
+function articleCreator(headline,authorImg,name) {
+    const card = document.createElement('div');
+    const headLine = document.createElement('div');
+    const author = document.createElement('div');
+    const imgCont = document.createElement('div');
+    const img = document.createElement('img');
+    const authName = document.createElement('span');
+
+    headLine.textContent = headline;
+    img.src = authorImg;
+    authName.textContent = name;
+
+    card.appendChild(headLine);
+    card.appendChild(author);
+    author.appendChild(imgCont);
+    imgCont.appendChild(img);
+    card.appendChild(authName);
+
+    card.classList.add('card');
+    headLine.classList.add('headline');
+    author.classList.add('author');
+    imgCont.classList.add('img-container');
+
+    return card;
+}
+
+const cardsCont = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
+    console.log(response.data.articles);
+    response.data.articles.bootstrap.forEach(item => {
+        let newCard = articleCreator(item.headline, item.authorPhoto, item.authorName);
+        cardsCont.appendChild(newCard);
+    })
+    response.data.articles.javascript.forEach(item => {
+        let newCard = articleCreator(item.headline, item.authorPhoto, item.authorName);
+        cardsCont.appendChild(newCard);
+    })
+    response.data.articles.jquery.forEach(item => {
+        let newCard = articleCreator(item.headline, item.authorPhoto, item.authorName);
+        cardsCont.appendChild(newCard);
+    })
+    response.data.articles.node.forEach(item => {
+        let newCard = articleCreator(item.headline, item.authorPhoto, item.authorName);
+        cardsCont.appendChild(newCard);
+    })
+    response.data.articles.technology.forEach(item => {
+        let newCard = articleCreator(item.headline, item.authorPhoto, item.authorName);
+        cardsCont.appendChild(newCard);
+    })
+});
+
